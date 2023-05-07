0bec157d965d6480564d18f6d54b17949d00000b
Egyed József <jegyed50@gmail.com>
2023-05-07 07:39:01 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,21 @@
+input.onButtonPressed(Button.A, function () {
+	
+})
+led.setBrightness(25)
+music.setVolume(255)
+led.toggle(2, 2)
+let hangerő = 10
+let haghossz = 100
+music.playSoundEffect(music.createSoundEffect(
+WaveShape.Sine,
+5000,
+0,
+255,
+0,
+500,
+SoundExpressionEffect.None,
+InterpolationCurve.Linear
+), SoundExpressionPlayMode.UntilDone)
+basic.forever(function () {
+	
+})
